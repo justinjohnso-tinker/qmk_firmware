9f405088fa9b156cc9d8198ded3a08dd478f9b55
Justin Johnson <justinaugustejohnson@gmail.com>
2023-02-16 16:14:37 -0800

    update momoka ergo keymap

--- keyboards/momoka_ergo/keymaps/justinjohnso/keymap.c
+++ keyboards/momoka_ergo/keymaps/justinjohnso/keymap.c
@@ -29,20 +29,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        MO(_FN1), KC_LCTL,  KC_RCTL, KC_LALT, KC_RGUI,                                                         KC_UP,   KC_DOWN, KC_LEFT, KC_RGHT, MO(_FN1),
-                                                               KC_HOME, KC_END,     KC_LBRC, KC_RBRC,
-                                                                        KC_PGUP,    KC_GRV,
-                                                      KC_SPC,  KC_BSPC, KC_PGDN,    KC_EQL,  KC_ENT,  KC_SPC
+        KC_LCTL,  KC_LALT, KC_LGUI, KC_GRV,  MO(_FN1),                                                         KC_UP,   KC_DOWN, KC_LEFT, KC_RGHT, KC_RCTL,
+                                                               KC_HOME, KC_END,    KC_LBRC, KC_RBRC,
+                                                                        KC_PGUP,   KC_EQL,
+                                                      KC_SPC,  KC_BSPC, KC_PGDN,   MO(_FN1),KC_ENT,   KC_SPC
     ),
     [_FN1] = LAYOUT(
-        KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                          KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,
-        _______,  _______, _______, _______, _______, _______,                                        _______, _______, _______,  _______, _______, _______,
-        _______,  _______, _______, _______, _______, _______,                                        _______, _______, _______,  _______, _______, _______,
-        MO(_FN2), _______, _______, _______, _______, _______,                                        _______, _______, RGB_RMOD, RGB_MOD, RGB_TOG, MO(_FN2),
-        _______,  _______, _______, _______, _______,                                                          _______, RGB_VAI,  RGB_VAD, RGB_SAD, RGB_SAI,
-                                                               _______, KC_MUTE,    RGB_HUD, RGB_HUI,
+        KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        _______,  _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______, _______,                                        _______, KC_UP,   KC_DOWN, KC_LEFT, KC_RGHT, _______,
+        MO(_FN2), _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______, MO(_FN2),
+        _______,  _______, _______, _______, _______,                                                          _______, _______, _______, _______, _______,
+                                                               _______, KC_MUTE,    KC_MPRV, KC_MNXT,
                                                                         KC_VOLU,    _______,
-                                                      _______, _______, KC_VOLD,    _______, _______, _______
+                                                      _______, KC_MSTP, KC_VOLD,    _______, KC_MPLY, _______
     ),
     [_FN2] = LAYOUT(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                          KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, QK_BOOT, _______, _______,                                        _______, _______, QK_BOOT,  _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                                        _______, _______, RGB_RMOD, RGB_MOD, RGB_TOG, _______,
         _______, _______, _______, _______, _______,                                                          _______, RGB_VAI, RGB_VAD,  RGB_SAD, RGB_SAI,
-                                                              _______, KC_MUTE,    RGB_HUD, RGB_HUI,
-                                                                       KC_VOLU,    _______,
-                                                     _______, _______, KC_VOLD,    _______, _______, _______
+                                                              _______, _______,    RGB_HUD, RGB_HUI,
+                                                                       _______,    _______,
+                                                     _______, _______, _______,    _______, _______, _______
     )
 
 };
